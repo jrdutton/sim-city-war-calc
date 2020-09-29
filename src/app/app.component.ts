bc0fbadbb6df5a8bb68a98741ee7f7ad686267c1
James Dutton <james@dutton.io>
2020-09-29 21:44:18 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sswc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sim-city-war-calc';
+}
